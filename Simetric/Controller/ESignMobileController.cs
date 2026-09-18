8b93e1d80a895447e022da7b38bfd3f0eb10680f
Arleth Quintana <numer_w2oslz@NUMERICA>
2026-09-18 14:51:31 -0500

    Controllers

--- Simetric/Controller/ESignMobileController.cs
+++ Simetric/Controller/ESignMobileController.cs
@@ -167,7 +167,6 @@ public sealed class ESignMobileController : ControllerBase
             {
                 id = emisor.Codigo,
                 razonSocial = emisor.RazonSocial,
-                nombreTitular = validacion?.NombreTitular ?? emisor.RazonSocial,
                 ruc = emisor.Ruc,
                 email = emisor.Email,
                 telefono = emisor.Telefono,
